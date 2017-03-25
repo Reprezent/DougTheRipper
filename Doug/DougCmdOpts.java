a470abaff408484ff555a523b95911da13eebd1d
Richard Riedel <richardalexriedel@yahoo.com>
2017-03-25 13:15:27 -0400

    moving command line options

---
+++ Doug/DougCmdOpts.java
@@ -0,0 +1,40 @@
+package Doug;
+
+// Richard Riedel, Nick Redheffer, Zach Houston
+// Group: Doug
+// Spring 2017 CS 434
+// Programming Assignment 1b | Part 4
+// This is the driver program that emulates JohnTheRipper in
+// Java. This program uses the special format requested by 
+// Professor Max Schuchard, "[GroupName]:[ChallengeNonce]:[Password]".
+// Where the quotes are not included.
+
+
+import java.util.Base64;
+// import java.io.BufferedReader;
+// import java.io.FileNotFoundException;
+// import java.io.FileReader;
+// import java.io.IOException;
+// import java.security.MessageDigest;
+// import java.security.NoSuchAlgorithmException;
+// import java.util.Scanner;
+// import java.util.Vector;
+import java.lang.Integer;
+import java.lang.Thread;
+import Doug.DougSmasher;
+import Doug.DougHash;
+import Doug.DougDict;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Option.Builder;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.ParseException;
+
+public class DougCmdOpts
+{
+	
+
+}
